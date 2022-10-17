62af301bb8267d36ff7d85eeb51267ecb1cf87e2
Sebas21gt <sebasteriogt@gmail.com>
2022-10-16 21:05:22 -0400

    Lab02_Rompecabezas

---
+++ Laboratorios/Rompecabezas_Lab2.py
@@ -0,0 +1,106 @@
+# datos = estado
+class Nodo:
+    def __init__(self, datos, hijo=None):
+        self.datos = datos
+        self.hijos = []
+        self.padre = None
+        self.costo = None
+        self.set_hijo(hijo)
+        
+    def set_hijo(self, hijo):
+        if (hijo is not None):
+            self.hijos.append(hijo)
+            if self.hijos is not None:
+                for h in self.hijos:
+                    h.padre = self
+                
+    def get_hijos(self):
+        return self.hijos
+    
+    def set_padre(self, padre):
+        self.padre = padre
+        
+    def get_padre(self):
+        return self.padre
+
+    def set_datos(self, datos):
+        self.datos = datos
+    
+    def get_datos(self):
+        return self.datos
+    
+    def set_costo(self, costo):
+        self.costo = costo
+        
+    def get_costo(self):
+        return self.costo
+    
+    def equal(self, nodo):
+        if self.get_datos() == nodo.get_datos():
+            return True
+        else:
+            return False
+    
+    def en_lista(self, lista_nodos):
+        enlistado = False
+        for n in lista_nodos:
+            if self.equal(n):
+                enlistado = True
+        return enlistado
+    
+    def __str__(self):
+        return str(self.get_datos())
+
+#GONZALES TITO SEBASTIAN
+#Elabore un programa para resolver un rompecabezas lineal de 10 dígitos en base al revisado en clase.
+
+"""En este caso mi programa solo funciona hasta 7 digitos con una respuesta de 4min 
+Debido a la memoria de la computadora, esta implementado con 2 ciclos for para recorrer el rompecabezas y una lista FIFO
+ya que con las listas LIFO en este caso tarda mas"""
+
+
+def bpa(estado_inicio, estado_solucion):
+    resuelto = False
+    nodos_visitados = []
+    nodos_frontera = []
+    nodo_inicio = Nodo(estado_inicio)
+    nodos_frontera.append(nodo_inicio)
+    
+    while resuelto == False and len(nodos_frontera) != 0:
+        nodo_actual = nodos_frontera.pop(0)
+        nodos_visitados.append(nodo_actual)
+        if nodo_actual.get_datos() == estado_solucion:
+            resuelto = True
+            return nodo_actual
+        else:
+            #Bucle para recorrer 
+            for i in range(0, len(estado_solucion)-1):
+              elemento = estado_solucion[i]
+              for j in range(0, len(estado_inicio)-1):
+                indice = estado_inicio.index(elemento)
+                
+                hijo_datos = nodo_actual.get_datos().copy()
+                temp = hijo_datos[indice-1]
+                hijo_datos[indice-1] = hijo_datos[indice]
+                hijo_datos[indice] = temp
+                hijo = Nodo(hijo_datos)
+                
+                if not hijo.en_lista(nodos_visitados) and not hijo.en_lista(nodos_frontera):
+                    nodo_actual.set_hijo(hijo)
+                    nodos_frontera.append(hijo)
+
+if __name__ == "__main__":
+    estado_inicial = [7, 6, 5, 4, 3, 2, 1]
+    solucion = [1, 2, 3, 4, 5, 6, 7]
+    nodo_solucion = bpa(estado_inicial, solucion)
+
+    # mostrar resultado
+    resultado = []
+    nodo_actual = nodo_solucion
+    while nodo_actual.get_padre() is not None:
+        resultado.append(nodo_actual.get_datos())
+        nodo_actual = nodo_actual.get_padre()
+
+    resultado.append(estado_inicial)
+    resultado.reverse()
+    print(resultado)
